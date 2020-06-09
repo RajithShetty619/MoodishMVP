59813e7ad71aa88c245afcbcb75a62920ec3bc11
RajithShetty619 <59964971+RajithShetty619@users.noreply.github.com>
2020-06-09 11:26:12 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/moodish_mvp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moodish_mvp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/mainScreen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Food',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Restaurant',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+
+    void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex)
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            title: Text('Food'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant),
+            title: Text('Restaurant'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
