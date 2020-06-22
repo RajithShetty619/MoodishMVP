ca08b2cdd943c11398601c9b5c794619354d235b
Meliodas45 <mohitbrijeshsharma45@gmail.com>
2020-06-22 11:56:09 +0530

    'chang'

--- lib/screens/Restaurants/home.dart
+++ lib/screens/Restaurants/home.dart
@@ -10,76 +10,112 @@ class Restaurant extends StatefulWidget {
 }
 
 class _RestaurantState extends State<Restaurant> {
+  List<_Restaurants> rest = [
+    _Restaurants(
+        image: 'downtown_china.jpg',
+        name: 'Downtown China',
+        desc:
+        'Casual Dining - Chinese, Thai, Asian, Seafood, Momos, Beverages, Desserts \nAndheri Lokhandwala, Andheri West'),
+    _Restaurants(
+        image: 'family_tree.jpg',
+        name: 'Family Tree',
+        desc:
+        'Casual Dining - Biryani, Chinese, North Indian, Pizza, South Indian, Desserts \nGhatkopar East'),
+    _Restaurants(
+        image: 'frozen_bottle.jpg',
+        name: 'Frozen Bottle',
+        desc:
+        'Beverage Shop, Dessert Parlor - Beverages, Desserts, Ice Cream \nMatunga East'),
+    _Restaurants(
+        image: 'irish_house.png',
+        name: 'The Irish House',
+        desc: 'Pub, Casual Dining - European, American \nLower Parel'),
+    _Restaurants(
+        image: 'Mainland_China.jpg',
+        name: 'Mainland China',
+        desc:
+        'Casual Dining - Chinese, Asian, Sushi, Japanese, Thai \nSakinaka'),
+    _Restaurants(
+        image: 'pop_tates.jpg',
+        name: 'Pop Tates',
+        desc:
+        'Bar, Casual Dining - Continental, Chinese, Italian, Beverages \nLower Parel'),
+    _Restaurants(
+        image: 'tipsy_gipsy.jpg',
+        name: 'Tipsy Gipsy',
+        desc:
+        'Bar, Casual Dining - Continental, Mediterranean, Italian \nVeera Desai Area'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
+    return  Scaffold(
+    body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.white, Colors.blueAccent]
+                )
+            ),
             child: Column(
               children: <Widget>[
                 Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [Colors.white, Colors.blueAccent]
-                          )
-                        ),
-                  child: Column(
+                  alignment: Alignment(0.0, 0.0),
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Hungry, Pradyum?',
+                    style: TextStyle(
+                        fontSize: 30.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Container(
-                        alignment: Alignment(0.0, 0.0),
-                        padding: EdgeInsets.only(top: 10.0),
-                        child: Text(
-                          'Hungry, Pradyum?',
-                          style: TextStyle(
-                              fontSize: 30.0, fontWeight: FontWeight.bold),
-                        ),
+                        alignment: AlignmentDirectional.center,
+                        child: Icon(Icons.location_on),
                       ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              alignment: AlignmentDirectional.center,
-                              child: Icon(Icons.location_on),
-                            ),
-                            SizedBox(width: 5.0,),
-                            Container(
-                              alignment: AlignmentDirectional.center,
-                              child: Text(
-                                'Mumbai, Maharashtra',
-                                style: TextStyle(
-                                  fontSize: 15.0,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      SizedBox(width: 5.0,),
                       Container(
-//                          color: Colors.white,
-                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search),
-                            hintText: 'Search',
+                        alignment: AlignmentDirectional.center,
+                        child: Text(
+                          'Mumbai, Maharashtra',
+                          style: TextStyle(
+                            fontSize: 15.0,
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: 20.0,
-                      )
                     ],
                   ),
                 ),
+                Container(
+//                          color: Colors.white,
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: <Widget>[
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -114,7 +150,7 @@ class _RestaurantState extends State<Restaurant> {
                                                 padding: EdgeInsets.only(top: 20.0),
                                                 child: Icon(
                                                   Icons.restaurant,
-                                                  size: 75,
+                                                  size: 75.0,
                                                   color: Colors.white,
                                                 ),
                                               ),
@@ -161,7 +197,7 @@ class _RestaurantState extends State<Restaurant> {
                                         padding: EdgeInsets.only(top: 20.0),
                                         child: Icon(
                                           Icons.calendar_today,
-                                          size: 125.0,
+                                          size: 75.0,
                                         ),
                                       ),
                                       Container(
@@ -216,7 +252,7 @@ class _RestaurantState extends State<Restaurant> {
                                                 padding: EdgeInsets.only(top: 20.0),
                                                 child: Icon(
                                                   Icons.location_on,
-                                                  size: 125.0,
+                                                  size: 75.0,
                                                   color: Colors.green,
                                                 ),
                                               ),
@@ -263,7 +299,7 @@ class _RestaurantState extends State<Restaurant> {
                                         padding: EdgeInsets.only(top: 20.0),
                                         child: Icon(
                                           Icons.stars,
-                                          size: 125.0,
+                                          size: 75.0,
                                           color: Colors.yellow,
                                         ),
                                       ),
@@ -285,13 +321,52 @@ class _RestaurantState extends State<Restaurant> {
                     ),
                     SizedBox(width: 10.0,)
 
+
                   ],
                 ),
+                SizedBox(height: 10.0,),
+                Text('Top Restaurants :',style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+                  textAlign: TextAlign.left,),
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: rest.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                        padding: EdgeInsets.only(top: 15.0),
+                        child: Card(
+                            child: ListTile(
+                              onTap: () {
+                                debugPrint('tappeed');
+                              },
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                AssetImage('assets/${rest[index].image}'),
+                              ),
+                              title: Text(rest[index].name),
+                              subtitle: Text(rest[index].desc),
+                            )));
+                  },
+                )
+
               ],
             ),
-          ),
-        ),
-      )),
-    ));
+          )
+
+        ],
+      ),
+    ),
+      );
   }
+}
+
+class _Restaurants {
+  String image;
+  String name;
+  String desc;
+
+  _Restaurants({this.image, this.name, this.desc});
 }
