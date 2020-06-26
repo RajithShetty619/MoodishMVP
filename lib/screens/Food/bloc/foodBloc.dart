6fa6777bce226cf8c7ccf4018ed7c907e91dd82e
RajithShetty619 <59964971+RajithShetty619@users.noreply.github.com>
2020-06-26 16:23:58 +0530

    rajith

--- lib/screens/Food/bloc/foodBloc.dart
+++ lib/screens/Food/bloc/foodBloc.dart
@@ -3,26 +3,27 @@ import 'package:hive/hive.dart';
 import 'package:moodish_mvp/models/foodListModel.dart';
 import 'package:moodish_mvp/screens/Food/events/foodEvent.dart';
 
-class FoodBloc extends Bloc<FoodEvent, List<FoodListModel>> {
+class FoodBloc extends Bloc<FoodEvent, Map<String, List<FoodListModel>>> {
   @override
-  List<FoodListModel> get initialState => List<FoodListModel>();
+  Map<String, List<FoodListModel>> get initialState => {"0":[]};
 
   @override
-  Stream<List<FoodListModel>> mapEventToState(FoodEvent event) async* {
+  Stream<Map<String, List<FoodListModel>>> mapEventToState(
+      FoodEvent event) async* {
     switch (event.eventType) {
       case EventType.add:
-        List<FoodListModel> newstate = List.from(state);
+        Map<String, List<FoodListModel>> newstate = state;
         if (event.food != null) {
           print("addinf");
-          newstate.addAll(event.food);
+          newstate["0"].addAll(event.food);
           final _box = Hive.box('foodlist');
-          _box.put('0', newstate);
+          _box.put('0', newstate["0"]);
         }
         yield newstate;
         break;
       case EventType.delete:
-        List<FoodListModel> newstate = List.from(state);
-        newstate.removeAt(event.foodIndex);
+        Map<String, List<FoodListModel>> newstate = state;
+        newstate[0].removeAt(event.foodIndex);
         yield newstate;
         break;
       default:
