394297a76ad3ed05c891a372fbac777ddea9362d
Meliodas45 <mohitbrijeshsharma45@gmail.com>
2020-07-04 20:46:30 +0530

    MM

--- lib/screens/Food/pages/foodFeed.dart
+++ lib/screens/Food/pages/foodFeed.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moodish_mvp/screens/Food/components/foodMood.dart';
 import 'package:moodish_mvp/screens/Food/pages/explore.dart';
 
 class FoodFeed extends StatefulWidget {
@@ -59,7 +60,65 @@ class _FoodFeedState extends State<FoodFeed> {
                             // ),
                           ],
                         ),
-                      ])
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: Container(
+                                width: 220.0,
+                                margin: EdgeInsets.all(10.0),
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.black, width: 2),
+                                  borderRadius: BorderRadius.circular(15),
+                                  // color: Colors.blue[200],
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Text(
+                                    'Top 10 for Your Mood',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 60,
+                              child: Text(
+                                'Happy',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                )
+                              ),
+                            ),
+                            SizedBox(width: 5)
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height:350,
+
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 10,
+                              itemBuilder: (BuildContext context,int index){
+                                return Mood_Food(image: 'assets/Chocolate.jpg', descrip1: 'food', descrip2: 'desc');
+                              }
+                            ),
+                          ),
+                        )
+                      ]
+                    )
                 ],
               ),
             )
@@ -69,3 +128,4 @@ class _FoodFeedState extends State<FoodFeed> {
     );
   }
 }
+
