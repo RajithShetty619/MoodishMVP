dc46280bc19a4c7c48a9864ddb5dd4102999656c
RajithShetty619 <59964971+RajithShetty619@users.noreply.github.com>
2020-07-22 20:16:03 +0530

    rajith

--- lib/test.dart
+++ lib/test.dart
@@ -2,6 +2,7 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
+import 'package:moodish_mvp/Services/searchFunction.dart';
 import 'package:moodish_mvp/screens/Food/bloc/foodBloc.dart';
 
 class Test extends StatefulWidget {
@@ -39,7 +40,7 @@ class _FoodListState extends State<FoodList> {
       .getHttpsCallable(functionName: 'helloWorld')
         ..timeout = const Duration(seconds: 30);
   int _responseCount = 0;
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -47,18 +48,8 @@ class _FoodListState extends State<FoodList> {
         child: FlatButton(
           onPressed: () async {
             try {
-              final HttpsCallableResult result = await callable.call(
-                <String, dynamic>{
-                  'message': 'hello world!',
-                  'count': _responseCount,
-                },
-              );
-              print(result.data);
-            } on CloudFunctionsException catch (e) {
-              print('caught firebase functions exception');
-              print(e.code);
-              print(e.message);
-              print(e.details);
+               dynamic res = await SearchFunction().search();
+               print(res);
             } catch (e) {
               print('caught generic exception');
               print(e);
