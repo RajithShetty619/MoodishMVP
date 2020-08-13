53de7b4ec57cee9e3cdada407afed2ed10b9d564
Testing <testing@Keshavs-MacBook-Pro.local>
2020-08-13 18:03:02 +0530

    ios_noti

--- lib/screens/Food/blocs/pollsbloc/pollsBloc.dart
+++ lib/screens/Food/blocs/pollsbloc/pollsBloc.dart
@@ -2,29 +2,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moodish_mvp/models/pollsModel.dart';
 import 'package:moodish_mvp/screens/Food/events/pollsEvent.dart';
 
-class PollBloc extends Bloc<PollEvent, Map<String, List<PollsModel>>>{
+class PollBloc extends Bloc<PollEvent, Map<String, List<dynamic>>> {
   @override
   // TODO: implement initialState
-  Map<String, List<PollsModel>> get initialState => {
-    "p": [],
-  };
+  Map<String, List<dynamic>> get initialState => {
+        "p": [],
+      };
 
   @override
-  Stream<Map<String, List<PollsModel>>> mapEventToState(PollEvent event) async* {
-    switch(event.eventType){
+  Stream<Map<String, List<dynamic>>> mapEventToState(PollEvent event) async* {
+    switch (event.eventType) {
       case EventType.add:
-     Map<String, List<PollsModel>> newstate = state;
-      if(event.polls != null) {
-        print('polllllllll');
-        print(event.polls);
-        newstate[event.listName].addAll(event.polls);
-        print(newstate);
-      }
-      yield newstate;
-      break;
+        Map<String, List<dynamic>> newstate = state;
+        if (event.polls != null) {
+          print('polllllllll');
+          print(event.polls);
+          newstate[event.listName].addAll(event.polls);
+          print(newstate);
+        }
+        yield newstate;
+        break;
       default:
-      throw Exception("Event not found : ${event}");
+        throw Exception("Event not found : ${event}");
     }
   }
-
 }
