f9687f831bb2e71713434e383ed3bffb7556437b
Rajith Shetty <shettyrajith.rs@gmail.com>
2020-09-12 12:05:30 +0530

    rajith

--- lib/Services/betaCount.dart
+++ lib/Services/betaCount.dart
@@ -3,10 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class BetaCount {
   count({String field}) {
     DocumentReference documentReference =
-        Firestore.instance.collection("betacount").document("$field");
+        FirebaseFirestore.instance.collection("betacount").document("$field");
 
     documentReference.setData({
       "count": FieldValue.increment(1) /* atomically increments data by 1 */
-    }, merge: true).catchError((onError) => print(onError));
+    }, SetOptions(merge: true)).catchError((onError) => print(onError));
   }
 }
