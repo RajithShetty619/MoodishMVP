c5bb5983aa1c127e50a59bf96422e3d1fcc1d219
Meliodas45 <mohitbrijeshsharma45@gmail.com>
2020-09-13 21:22:29 +0530

    _

--- lib/screens/Restaurants/restaurantCard/overview.dart
+++ lib/screens/Restaurants/restaurantCard/overview.dart
@@ -18,19 +18,127 @@ class _OverViewState extends State<OverView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-      children: <Widget>[
-        Expanded(
-            child: Row(
-          children: <Widget>[
-            IconButton(
-                icon: Icon(Icons.call),
-                onPressed: () {
-                  launch(('tel:${widget.rest.international_phone_number}'));
-                })
-          ],
-        ))
-      ],
-    ));
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              widget.rest.international_phone_number == ''
+                  ? Container()
+                  : Container(
+                      child: GestureDetector(
+                          onTap: () {
+                            launch(
+                                ('tel:${widget.rest.international_phone_number}'));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Card(
+                              child: Row(
+                                children: [
+                                  Container(
+                                    child: IconButton(
+                                      iconSize: 40,
+                                      icon: Icon(Icons.call),
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 7.0,
+                                  ),
+                                  Container(
+                                      margin: EdgeInsets.all(5),
+                                      child: Text(
+                                        '${widget.rest.international_phone_number}',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            ),
+                          )),
+                    ),
+              widget.rest.website == ''
+                  ? Container()
+                  : Container(
+                      child: GestureDetector(
+                          onTap: () {
+                            launch(('${widget.rest.website}'));
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    child: IconButton(
+                                        iconSize: 40,
+                                        icon: Icon(Icons.language),
+                                        onPressed: () {
+                                          launch(('${widget.rest.website}'));
+                                        }),
+                                  ),
+                                  SizedBox(
+                                    height: 7.0,
+                                  ),
+                                  Container(
+                                      child: GestureDetector(
+                                          onTap: () {
+                                            launch(('${widget.rest.website}'));
+                                          },
+                                          child: Text(
+                                            '${widget.rest.website}',
+                                            style: TextStyle(
+                                              fontSize: 20
+                                            ),
+                                          ))),
+                                ],
+                              ),
+                            ),
+                          )),
+                    ),
+                    Container(
+                      child: GestureDetector(
+                          onTap: () {
+
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    child: IconButton(
+                                        iconSize: 40,
+                                        icon: Icon(Icons.location_on),
+                                        onPressed: () {
+
+                                        }),
+                                  ),
+                                  SizedBox(
+                                    height: 7.0,
+                                  ),
+                                  Container(
+                                      child: Text(
+                                        '${widget.rest}',
+                                        style: TextStyle(
+                                          fontSize: 20
+                                        ),
+                                      )),
+                                ],
+                              ),
+                            ),
+                          )),
+                    ),
+            ],
+          ),
+        ));
   }
 }
+// IconButton(
+//     icon: Icon(Icons.call),
+//     onPressed: () {
+//       launch(('tel:${widget.rest.international_phone_number}'));
+//     })
