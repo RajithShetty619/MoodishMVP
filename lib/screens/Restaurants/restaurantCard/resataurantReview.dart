4eee3595e01832a0c4af6348cf9ab6d81f820cb3
Testing <testing@Keshavs-MacBook-Pro.local>
2020-09-28 11:08:21 +0530

    ios

--- lib/screens/Restaurants/restaurantCard/resataurantReview.dart
+++ lib/screens/Restaurants/restaurantCard/resataurantReview.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:moodish_mvp/Services/authenticate.dart';
+import 'package:moodish_mvp/Services/database.dart';
 import 'package:moodish_mvp/models/restaurantsModel.dart';
 
 class RestaurantReview extends StatefulWidget {
@@ -39,12 +41,15 @@ class _RestaurantReviewState extends State<RestaurantReview> {
                   alignment: Alignment.center,
                   child: Padding(
                     padding: EdgeInsets.only(
-                        left: 0,),
-                    child: Text('Reviews',
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),),
+                      left: 0,
+                    ),
+                    child: Text(
+                      'Reviews',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
                   ),
                 ),
               ],
@@ -82,20 +87,21 @@ class _RestaurantReviewState extends State<RestaurantReview> {
                                     fontSize: 24,
                                     color: Colors.white),
                               ),
-                              widget.rest.rating=='5'?
-                              Text(
-                                '\u{02605}${widget.rest.rating}.0',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                    color: Colors.white),
-                              ):                              Text(
-                                '\u{02605}${widget.rest.rating}',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                    color: Colors.white),
-                              ),
+                              widget.rest.rating == '5'
+                                  ? Text(
+                                      '\u{02605}${widget.rest.rating}.0',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                          color: Colors.white),
+                                    )
+                                  : Text(
+                                      '\u{02605}${widget.rest.rating}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                          color: Colors.white),
+                                    ),
                             ],
                           ),
                         ),
@@ -140,7 +146,7 @@ class _RestaurantReviewState extends State<RestaurantReview> {
                   Padding(
                     padding: const EdgeInsets.only(left: 5.0, right: 5.0),
                     child: TextField(
-                      controller: _textController,
+                        controller: _textController,
                         maxLines: 5,
                         onChanged: (val) {
                           review.add(val);
@@ -200,6 +206,141 @@ class _RestaurantReviewState extends State<RestaurantReview> {
                                   Spacer(),
                                   Text(
                                       '\u{02605}${widget.rest.reviews[index]["rating"]}'),
+                                  IconButton(
+                                      icon: Icon(Icons.info_outline),
+                                      onPressed: () async {
+                                        bool visible = false;
+                                        String description = '';
+                                        dynamic data = await showDialog(
+                                          context: context,
+                                          builder: (dialogcontext) {
+                                            return AlertDialog(
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                              ),
+                                              title: Text(
+                                                  "Why do you want to report this?"),
+                                              content: Column(
+                                                children: [
+                                                  FlatButton(
+                                                      onPressed: () async {
+                                                        await DatabaseService()
+                                                            .report(
+                                                                rest:
+                                                                    widget.rest,
+                                                                index: index,
+                                                                uid: Authenticate()
+                                                                    .returnUid())
+                                                            .then((value) {
+                                                          Navigator.pop(
+                                                              dialogcontext,
+                                                              "done");
+                                                        });
+                                                      },
+                                                      child: Align(
+                                                        child: Text("Its Spam"),
+                                                        alignment: Alignment
+                                                            .centerLeft,
+                                                      )),
+                                                  FlatButton(
+                                                      onPressed: () {
+                                                        setState(() {
+                                                          visible = true;
+                                                        });
+                                                      },
+                                                      child: Align(
+                                                        child: Text("Other:"),
+                                                        alignment: Alignment
+                                                            .centerLeft,
+                                                      )),
+                                                  // if (visible)
+                                                  Column(
+                                                    children: [
+                                                      Padding(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                    .fromLTRB(
+                                                                15, 0.0, 5, 6),
+                                                        child: TextField(
+                                                            onChanged: (val) {
+                                                              setState(() {
+                                                                description =
+                                                                    val;
+                                                              });
+                                                            },
+                                                            decoration:
+                                                                InputDecoration(
+                                                              hintText:
+                                                                  'Describe why its inapropriate?',
+                                                              enabledBorder:
+                                                                  OutlineInputBorder(
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            5.0),
+                                                              ),
+                                                              focusedBorder:
+                                                                  OutlineInputBorder(
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            30.0),
+                                                              ),
+                                                            )),
+                                                      ),
+                                                      Align(
+                                                        alignment: Alignment
+                                                            .centerRight,
+                                                        child: RaisedButton(
+                                                          onPressed: () {
+                                                            DatabaseService()
+                                                                .report(
+                                                                    rest: widget
+                                                                        .rest,
+                                                                    index:
+                                                                        index,
+                                                                    description:
+                                                                        description,
+                                                                    uid: Authenticate()
+                                                                        .returnUid())
+                                                                .then((value) {
+                                                              Navigator.pop(
+                                                                  dialogcontext,
+                                                                  "done");
+                                                            });
+                                                          },
+                                                          child: Text("ok"),
+                                                        ),
+                                                      )
+                                                    ],
+                                                  )
+                                                ],
+                                              ),
+                                              actions: [
+                                                FlatButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(
+                                                        dialogcontext);
+                                                  },
+                                                  child: Text(
+                                                    "Cancel",
+                                                    style: TextStyle(
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            );
+                                          },
+                                        );
+                                        if (data == "done") {
+                                          Scaffold.of(context).showSnackBar(
+                                              SnackBar(
+                                                  content: Text(
+                                                      "Thank you for your feedback")));
+                                        }
+                                      })
                                 ],
                               ),
                               subtitle:
