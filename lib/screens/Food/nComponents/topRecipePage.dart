af32da4a3145ab4e30af6876c7a517414ce7cf81
MohitSh45 <mohitbrijeshsharma45@gmail.com>
2021-01-21 00:05:25 +0530

    Final

--- lib/screens/Food/nComponents/topRecipePage.dart
+++ lib/screens/Food/nComponents/topRecipePage.dart
@@ -551,7 +551,7 @@ class TopCard extends StatelessWidget {
                     ),
                   )
                 ],
-              )
+              ),
             ],
           ),
           ClipRRect(
